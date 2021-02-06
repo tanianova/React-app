36c2d65365e4f1693f68f56c01da84d6ac241895
Tatiana <tanianova@yandex.ru>
2021-02-06 15:40:24 +0300

    added styles for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,21 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div >
-            dialogs
+        <div>
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' ' + s.active}>user1</div>
+                    <div className={s.dialog}>user2</div>
+                    <div className={s.dialog}>user3</div>
+                    <div className={s.dialog}>user4</div>
+                    <div className={s.dialog}>user5</div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>hi</div>
+                    <div className={s.message}>how are you?</div>
+                    <div className={s.message}>yo</div>
+                </div>
+            </div>
         </div>
     )
 }
