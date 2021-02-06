b2b70fdafc4faf6b13df0d2c979493305b5b8fa6
Tatiana <tanianova@yandex.ru>
2021-02-06 14:04:05 +0300

    updated css styles, added Routes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
+import React from 'react';
 import s from './Navbar.module.css';
 
 const Navbar =() => {
   return (
 <nav className={s.nav}>
      <div className={`${s.item} ${s.active}`}>
-     <a>Profile</a>
+     <a href="/profile">Profile</a>
      </div>
      <div className={s.item}>
-         <a>Messages</a>
+         <a href="/dialogs">Messages</a>
      </div>
      <div className={s.item}>
-         <a>News</a>
+         <a href="/news">News</a>
      </div>
      <div className={s.item}>
-         <a>Music</a>
+         <a href="/music">Music</a>
      </div>
      <div className={s.item}>
-         <a>Settings</a>
+         <a href="/settings">Settings</a>
      </div>
      </nav>
   )
