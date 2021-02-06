ca251a8ab680508563f246b710a4c0a4aa5b91f6
Tatiana <tanianova@yandex.ru>
2021-02-06 11:22:15 +0300

    created component dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts'
 
 const Profile =() => {
   return (
-<div className={s.content}>
+<div>
      <div>
-       <img src = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'/>
+       <img src = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg' alt='img'/>
        </div>
      <div>
        ava + description
