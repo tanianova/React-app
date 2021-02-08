c37e0ec1bec87c79ddd3fc5b9a9e9e9ff60ebc66
Tatiana <tanianova@yandex.ru>
2021-02-08 17:18:55 +0300

    created object dispatch in state.js, moved 'add post', etc. in dispatch, changed functions in MyPosts

--- src/App.js
+++ src/App.js
@@ -21,14 +21,11 @@ const App = (props) => {
                 <Route path='/dialogs'
                        render={() => <Dialogs
                            state={props.state.dialogsPage}
-                           addMessage={props.addMessage}
-                           updateNewMessageText={props.updateNewMessageText}/>}/>
+                           dispatch={props.dispatch}/>}/>
                 <Route path='/profile'
                        render={() => <Profile
                            profilePage={props.state.profilePage}
-                           addPost={props.addPost}
-                           updateNewPostText={props.updateNewPostText}
-                       />}/>
+                           dispatch={props.dispatch}/>}/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
                 <Route path='/settings' render={() => <Settings/>}/>
--- src/redux/state.js
+++ src/redux/state.js
@@ -29,42 +29,43 @@ let store = {
             }
         },
     },
-    getState() {
-        return this._state;
-    },
     _callSubscriber() {
         console.log('state was changed');
     },
-    addPost() {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0
-        };
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state);
 
-    },
-    updateNewPostText(newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
+    getState() {
+        return this._state;
     },
     subscribe(observer) {
         this._callSubscriber = observer;
     },
-    addMessage() {
-        let newMessage = {
-            id: 1,
-            message: this._state.dialogsPage.newDialog.message
-        };
-        this._state.dialogsPage.messages.push(newMessage);
-        this._state.dialogsPage.newDialog.message = '';
-        this._callSubscriber(this._state);
-    },
-    updateNewMessageText(newMessage) {
-        this._state.dialogsPage.newDialog.message = newMessage;
-        this._callSubscriber(this._state);
+
+    dispatch(action) {
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            };
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        } else if
+        (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        } else if (action.type === 'ADD-MESSAGE') {
+            let newMessage = {
+                id: 1,
+                message: this._state.dialogsPage.newDialog.message
+            };
+            this._state.dialogsPage.messages.push(newMessage);
+            this._state.dialogsPage.newDialog.message = '';
+            this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE-NEW-MESSAGE-TEXT') {
+            this._state.dialogsPage.newDialog.message = action.newMessage;
+            this._callSubscriber(this._state);
+        }
     }
 }
 
